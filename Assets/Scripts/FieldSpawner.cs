65e8ed401340b9b64375c72b84f6d9f74969b2bd
brakmann <evgeniybrakmann@gmail.com>
2024-11-08 14:49:23 +0300

    Correct map spawning

--- Assets/Scripts/FieldSpawner.cs
+++ Assets/Scripts/FieldSpawner.cs
@@ -1,26 +1,64 @@
+using System.Collections.Generic;
+using Unity.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class FieldSpawner : MonoBehaviour
 {
-    [SerializeField] GameObject closedTilePrefab;
-    struct Point {public int x, y;}
+    public struct Point {public int x, y;}
+    private readonly float tileSize = 0.07f;
+    private HashSet<Point> bombPositions;
+
+    [SerializeField] GameObject tilePrefab;
     public Vector2 startingCoordinate = new Vector2(0, 0);
-    public float tileSize = 0.07f;
+    public int bombsToSpawn;
+    public Point fieldSize = new Point{x = 10, y=10};
+
     void Start()
     {
-        SpawnOutterLayer(new Point{x = 10, y = 10}, startingCoordinate);
+        SpawnField(bombsToSpawn, fieldSize, startingCoordinate);
     }
 
-    void SpawnOutterLayer(Point size, Vector2 startingCoordinate) {
-        for (int i = 0; i<size.x; i++) {
-            for (int j = 0; j<size.y; j++) {
+    void SpawnField(int bombsNumber, Point fieldSize, Vector2 startingCoordinate) {
+        HashSet<Point> bombsLocation = RandomizeBombsLocation(bombsNumber, fieldSize);
+        //spawnObjects
+        for (int i = 0; i<fieldSize.x; i++) {
+            for (int j = 0; j<fieldSize.y; j++) {
+                int bombsAround = 0;
                 Vector2 coordinate = startingCoordinate + new Vector2(i * tileSize, j * tileSize);
-                Instantiate(closedTilePrefab, coordinate, closedTilePrefab.transform.rotation);
+                GameObject newTile = Instantiate(tilePrefab, coordinate, tilePrefab.transform.rotation);
+                if (bombsLocation.Contains(new Point{x = i, y = j})) {
+                    newTile.GetComponent<Tile>().SetBomb();
+                } else {
+                    //I'm not sure if this right, but i don't have coordinates of tiles and don't want to use colliders
+                    if (bombsLocation.Contains(new Point{x = i-1, y = j-1}))
+                        bombsAround++;
+                    if (bombsLocation.Contains(new Point{x = i-1, y = j}))
+                        bombsAround++;
+                    if (bombsLocation.Contains(new Point{x = i-1, y = j+1}))
+                        bombsAround++;
+                    if (bombsLocation.Contains(new Point{x = i, y = j-1}))
+                        bombsAround++;
+                    if (bombsLocation.Contains(new Point{x = i, y = j+1}))
+                        bombsAround++;
+                    if (bombsLocation.Contains(new Point{x = i+1, y = j-1}))
+                        bombsAround++;
+                    if (bombsLocation.Contains(new Point{x = i+1, y = j}))
+                        bombsAround++;
+                    if (bombsLocation.Contains(new Point{x = i+1, y = j+1}))
+                        bombsAround++;
+                    newTile.GetComponent<Tile>().SetBombsAround(bombsAround);
+                }
             }
         }
     }
-    void SpawnInnerLayer(Point size) {
-        
+
+    private HashSet<Point> RandomizeBombsLocation (int numberOfBombs, Point fieldSize) {
+        HashSet<Point> bombsLocation = new HashSet<Point>();
+        while (bombsLocation.Count < numberOfBombs) {
+            Point pointToAdd = new Point{x = Random.Range(0, fieldSize.x), y = Random.Range(0, fieldSize.y)};
+            bombsLocation.Add(pointToAdd);
+        }
+        return bombsLocation;
     }
 }
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,87 @@
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    private bool isOpened = false;
+    private bool isFlagged = false;
+    private bool hasBomb;
+    private int bombsAround;
+    [SerializeField] Sprite empty0Image;
+    [SerializeField] Sprite empty1Image;
+    [SerializeField] Sprite empty2Image;
+    [SerializeField] Sprite empty3Image;
+    [SerializeField] Sprite empty4Image;
+    [SerializeField] Sprite empty5Image;
+    [SerializeField] Sprite empty6Image;
+    [SerializeField] Sprite empty7Image;
+    [SerializeField] Sprite empty8Image;
+    [SerializeField] Sprite bombImage;
+    [SerializeField] Sprite notFlaggedImage;
+    [SerializeField] Sprite flaggedImage;
+
+    void OnMouseOver(){
+        //Left click
+        if(Input.GetMouseButtonDown(0)) {
+            if (!isOpened)
+                Open();
+        }
+        //Right click
+        if(Input.GetMouseButtonDown(1)) {
+            if (!isOpened)
+                ToggleFlag();
+        } 
+    }
+
+    //Inizialization
+    public void SetBomb () {
+        hasBomb = true;
+    }
+    public void SetBombsAround (int bombsNumber) {
+        bombsAround = bombsNumber;
+    }
+
+    //Game methods
+    void ToggleFlag() {
+        isFlagged = !isFlagged;
+        if (isFlagged) {
+            GetComponent<SpriteRenderer>().sprite = flaggedImage;
+        } else {
+            GetComponent<SpriteRenderer>().sprite = notFlaggedImage;
+        }            
+    }
+    void Open() {
+        if (hasBomb) {
+            GetComponent<SpriteRenderer>().sprite = bombImage;
+        } else {
+        switch (bombsAround) {
+            case 0: 
+                GetComponent<SpriteRenderer>().sprite = empty0Image;
+                break;
+            case 1: 
+                GetComponent<SpriteRenderer>().sprite = empty1Image;
+                break;
+            case 2: 
+                GetComponent<SpriteRenderer>().sprite = empty2Image;
+                break;
+            case 3: 
+                GetComponent<SpriteRenderer>().sprite = empty3Image;
+                break;
+            case 4: 
+                GetComponent<SpriteRenderer>().sprite = empty4Image;
+                break;
+            case 5: 
+                GetComponent<SpriteRenderer>().sprite = empty5Image;
+                break;
+            case 6: 
+                GetComponent<SpriteRenderer>().sprite = empty6Image;
+                break;
+            case 7: 
+                GetComponent<SpriteRenderer>().sprite = empty7Image;
+                break;
+            case 8: 
+                GetComponent<SpriteRenderer>().sprite = empty8Image;
+                break;
+        }
+        }
+    }
+}
